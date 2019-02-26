6cefa5254e0a5ab9ce97b87e96d2abc3bef39792
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-02-26 16:25:28 +0100

    switchen tussen verschillende schermen
    Ik heb een beetje zitten oefenen dmv van tutorials met verschillende schermen, ook al was dat niet mijn deel van de verschillende prototypes, ik ben nu bezig met sql lite

--- De Open dag app/app/src/main/java/com/example/deopendagapp/MainActivity.java
+++ De Open dag app/app/src/main/java/com/example/deopendagapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.deopendagapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Tweede Scherm met als tekst Hello world! openen.
+        Button btnSecondActivity = (Button)findViewById(R.id.btnSecondActivity);
+        btnSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.deopendagapp.tekstommeetetesten", "Hello World!");
+                startActivity(startIntent);
+            }
+        });
+
+        // Google.com openen
+
+        Button btnGoogle = (Button) findViewById(R.id.btnGoogle);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String google = "https://www.google.com";
+                Uri webadress = Uri.parse(google);
+
+                Intent googleIntent = new Intent(Intent.ACTION_VIEW, webadress);
+                if (googleIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(googleIntent);
+                }
+            }
+        });
     }
 }
