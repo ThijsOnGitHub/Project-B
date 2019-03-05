7bd6676361360eeab7e160395ba8239b0b9d225f
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-05 16:17:00 +0100

    Meer overzicht toegevoegd in databasehelper

--- De Open dag app/app/src/main/java/com/example/deopendagapp/DatabaseHelper.java
+++ De Open dag app/app/src/main/java/com/example/deopendagapp/DatabaseHelper.java
@@ -19,50 +19,64 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             -------------------------------------
     */
 
+    private  static final int DATABASE_VERSION = 2; // when updating structure increase by 1
+    private static final String DATABASE_NAME = "student.db";
+        public static final String TABLE_NAME_1 = "student_table";
+            public static final String COL_1 = "ID";
+            public static final String COL_2 = "NAME";
+            public static final String COL_3 = "SURNAME";
+            public static final String COL_4 = "MARKS";
 
-    public static final String DATABASE_NAME = "student.db";
-    public static final String TABLE_NAME = "student_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "SURNAME";
-    public static final String COL_4 = "MARKS";
+
 
     // CREATE DATABASE WHEN CALLED
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     // CREATE TABLE WHEN CALLED
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+        String query = "CREATE TABLE " + TABLE_NAME_1 + "(" +
+                COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_2 + " TEXT, " +
+                COL_3 + " TEXT, " +
+                COL_4 + " INTEGER" +
+                ")";
+
+        db.execSQL(query);
     }
 
     // DELETE TABLE AND CREATE AGAIN WHEN CALLED
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_1);
         onCreate(db);
     }
 
+
     public boolean insertData(String col2_value, String col3_value, String col4_value) {
         SQLiteDatabase db = this.getWritableDatabase();
+
+        // columns, values stored in contentvalues
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, col2_value);
         contentValues.put(COL_3, col3_value);
         contentValues.put(COL_4, col4_value);
-        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        long result = db.insert(TABLE_NAME_1, null, contentValues);
 
         if (result == -1) {
-            return false;
+            return false; // error, not inserted
         } else {
-            return true;
+            return true; // inserted
         }
     }
 
     public Cursor getAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        Cursor res = db.rawQuery("SELECT * FROM " + TABLE_NAME_1, null);
         return res;
     }
 }
