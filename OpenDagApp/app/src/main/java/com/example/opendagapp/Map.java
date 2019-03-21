2e9a1072d9cdcbef086f37bab1a0fac682594884
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-21 17:46:55 +0100

    Create simple beginning of floor buttons

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
@@ -2,12 +2,34 @@ package com.example.opendagapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Map extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //https://www.youtube.com/watch?v=SLFrwl1hFcw
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        ListView floorChooser = (ListView)findViewById(R.id.Listview_Floors_Floor_Chooser);
+        reloadFloors(7,floorChooser);
+
+
     }
+
+    public void reloadFloors(int amountOfFloors,ListView floorChooser) {
+        ArrayList<String> floors = new ArrayList<>();
+        for (int i = amountOfFloors; i >-1; i--) {
+            floors.add(Integer.toString(i));
+        }
+        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,floors);
+        floorChooser.setAdapter(adapter);
+    };
+
 }
