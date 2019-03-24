55682497b18e3658682c7ff8272f4a91919eedf9
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-24 17:40:53 +0100

    Niks gedaan

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
@@ -1,22 +1,33 @@
 package com.example.opendagapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.util.Log;
+import android.widget.ImageView;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 public class Map extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //https://www.youtube.com/watch?v=SLFrwl1hFcw
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        ImageView showFloor = findViewById(R.id.showFloor);
+
+
+
+    }
+        /*
+
+        Version 2
+        //https://www.youtube.com/watch?v=SLFrwl1hFcw
+
         ListView floorChooser = (ListView)findViewById(R.id.Listview_Floors_Floor_Chooser);
         reloadFloors(7,floorChooser);
 
@@ -31,5 +42,5 @@ public class Map extends AppCompatActivity {
         ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,floors);
         floorChooser.setAdapter(adapter);
     };
-
+*/
 }
