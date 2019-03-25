ac888885ebe0cde431ae16e52f55be063258c2f0
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-25 13:36:16 +0100

    branch opnieuw gedownload, android studio pakte m niet

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Contact.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Contact.java
@@ -1,13 +1,7 @@
 package com.example.opendagapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.os.Bundle;
 
 public class Contact extends AppCompatActivity {
 
@@ -16,14 +10,4 @@ public class Contact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
     }
-
-    // showMessage string title, string message (ALERT BOX)
-    public void showAlertMessage(String title, String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
-    }
-
 }
