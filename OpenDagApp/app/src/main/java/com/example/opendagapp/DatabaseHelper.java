995dbba36eb95be96246d65472d7e086bbef9d53
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-28 19:56:29 +0100

    Database setup errors

--- OpenDagApp/app/src/main/java/com/example/opendagapp/DatabaseHelper.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/DatabaseHelper.java
@@ -6,44 +6,121 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
     private static final int DB_DATABASE_VERSION = 1;
-    private static final String DB_DATABASE_NAME = "hro_opendays.db";
+    private static final String DB_DATABASE_HROOPENDAY = "hro_openday.db";
+        public static final String DB_TABLE_OPENDAY = "openday";
+            public static final String DB_TABLE_OPENDAY_ID = "id"; // Primary key, autoincrement
+            public static final String DB_TABLE_OPENDAY_DATE = "date"; // text
+            public static final String DB_TABLE_OPENDAY_STARTTIME = "start_time"; // text
+            public static final String DB_TABLE_OPENDAY_ENDTIME = "end_time"; // text
+            public static final String DB_TABLE_OPENDAY_INSTITUTEFULLNAME = "institute_fullname"; // text
+
         public static final String DB_TABLE_INSTITUTE = "institute";
             public static final String DB_TABLE_INSTITUTE_ID = "id"; // Primary key, autoincrement
-            public static final String DB_TABLE_INSTITUTE_NAME = "name"; // Text
-            public static final String DB_TABLE_INSTITUTE_CONTACTINFO = "contact_info"; // Text
-            public static final String DB_TABLE_INSTITUTE_TEXT = "text"; // Text
-            public static final String DB_TABLE_INSTITUTE_DATE = "date"; // Text
-        public static final String DB_TABLE_INSTITUTEADDRESS = "institute_address";
-            public static final String DB_TABLE_INSTITUTEADDRESS_ID = "id"; // Primary key, autoincrement
-            public static final String DB_TABLE_INSTITUTEADDRESS_INSTITUTEID = "institute_id"; // int
-            public static final String DB_TABLE_INSTITUTEADDRESS_ADDRESS = "address"; // text
-            public static final String DB_TABLE_INSTITUTEADDRESS_NUMBERFLOORS = "number_floors"; // int
-        public static final String DB_TABLE_STUDY = "study_programs";
+            public static final String DB_TABLE_INSTITUTE_FULLNAME = "full_name"; // text
+            public static final String DB_TABLE_INSTITUTE_SHORTNAME = "short_name"; // text
+            public static final String DB_TABLE_INSTITUTE_GENERALINFORMATION = "general_information"; // text
+
+        public static final String DB_TABLE_STUDY = "study";
             public static final String DB_TABLE_STUDY_ID = "id"; // Primary key, autoincrement
-            public static final String DB_TABLE_STUDY_STUDYNAME = "study_name"; // text
-            public static final String DB_TABLE_STUDY_STUDYTYPE = "study_type"; // text
-            public static final String DB_TABLE_STUDY_INSTITUTEID = "institute_id"; // int
-            public static final String DB_TABLE_STUDY_LOCATION = "location"; // text
-        public static final String DB_TABLE_STUDYACTIVITIES = "study_activities";
-            public static final String DB_TABLE_STUDYACTIVITIES_ID = "id"; // Primary key, autoincrement
-            public static final String DB_TABLE_STUDYACTIVITIES_STUDYID = "study_id"; // int
-            public static final String DB_TABLE_STUDYACTIVITIES_LOCATION = "location"; // text
-            public static final String DB_TABLE_STUDYACTIVITIES_TIME = "time"; // text
-            public static final String DB_TABLE_STUDYACTIVITIES_INFORMATION = "information"; // text
+            public static final String DB_TABLE_STUDY_INSTITUTEFULLNAME = "institute_fullname"; // text
+            public static final String DB_TABLE_STUDY_NAME = "name"; // text
+            public static final String DB_TABLE_STUDY_TYPE = "type"; // text
+            public static final String DB_TABLE_STUDY_LOCATIONADRESS = "location_address"; // text
+
+        public static final String DB_TABLE_LOCATION = "location";
+            public static final String DB_TABLE_LOCATION_ID = "id"; // Primary key, autoincrement
+            public static final String DB_TABLE_LOCATION_ADDRESS = "address"; // text
+            public static final String DB_TABLE_LOCATION_ZIPCODE = "zipcode"; // text
+            public static final String DB_TABLE_LOCATION_PHONENUMBER = "phone_number"; // text
+            public static final String DB_TABLE_LOCATION_IMAGEDESCRIPRION = "image_description"; // text
+
+        public static final String DB_TABLE_IMAGE = "image";
+            public static final String DB_TABLE_IMAGE_ID = "id"; // Primary key, autoincrement
+            public static final String DB_TABLE_IMAGE_FILENAME = "filename"; // text
+            public static final String DB_TABLE_IMAGE_CONTEXT = "context"; // text
+            public static final String DB_TABLE_IMAGE_DESCRIPTION = "description"; // text
+            public static final String DB_TABLE_IMAGE_FLOORNUMBER = "floor_number"; // int
+
+        public static final String DB_TABLE_ACTIVITY = "activity";
+            public static final String DB_TABLE_ACTIVITY_ID = "id"; // Primary key, autoincrement
+            public static final String DB_TABLE_ACTIVITY_STUDYNAME = "study_name"; // text
+            public static final String DB_TABLE_ACTIVITY_OPENDAYDATE = "openday_date"; // text
+            public static final String DB_TABLE_ACTIVITY_STARTTIME = "start_time"; // text
+            public static final String DB_TABLE_ACTIVITY_ENDTIME = "end_time"; // text
+            public static final String DB_TABLE_ACTIVITY_CLASSROOM = "classroom"; // text
+            public static final String DB_TABLE_ACTIVITY_INFORMATION = "information"; // text
 
     public DatabaseHelper(Context context) {
-        super(context, DB_DATABASE_NAME, null, DB_DATABASE_VERSION);
+        super(context, DB_DATABASE_HROOPENDAY, null, DB_DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + DB_TABLE_OPENDAY + "(" +
+                            DB_TABLE_OPENDAY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                            DB_TABLE_OPENDAY_DATE + " TEXT, " +
+                            DB_TABLE_OPENDAY_STARTTIME + " TEXT, " +
+                            DB_TABLE_OPENDAY_ENDTIME + " TEXT, " +
+                            DB_TABLE_OPENDAY_INSTITUTEFULLNAME + " TEXT" +
+                        ")";
+        db.execSQL(query);
 
+        query = "CREATE TABLE " + DB_TABLE_INSTITUTE + "(" +
+                    DB_TABLE_INSTITUTE_ID  + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DB_TABLE_INSTITUTE_FULLNAME + " TEXT, " +
+                    DB_TABLE_INSTITUTE_SHORTNAME + " TEXT, " +
+                    DB_TABLE_INSTITUTE_GENERALINFORMATION + " TEXT" +
+                ")";
+        db.execSQL(query);
+
+        query = "CREATE TABLE " + DB_TABLE_STUDY + "(" +
+                    DB_TABLE_STUDY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DB_TABLE_STUDY_INSTITUTEFULLNAME + " TEXT, " +
+                    DB_TABLE_STUDY_NAME + " TEXT, " +
+                    DB_TABLE_STUDY_TYPE + " TEXT, " +
+                    DB_TABLE_STUDY_LOCATIONADRESS + " TEXT" +
+                ")";
+        db.execSQL(query);
+
+        query = "CREATE TABLE " + DB_TABLE_LOCATION + "(" +
+                    DB_TABLE_LOCATION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DB_TABLE_LOCATION_ADDRESS + " TEXT, " +
+                    DB_TABLE_LOCATION_ZIPCODE + " TEXT, " +
+                    DB_TABLE_LOCATION_PHONENUMBER + " TEXT, " +
+                    DB_TABLE_LOCATION_IMAGEDESCRIPRION + " TEXT" +
+                ")";
+        db.execSQL(query);
+
+        query = "CREATE TABLE " + DB_TABLE_IMAGE + "(" +
+                    DB_TABLE_IMAGE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DB_TABLE_IMAGE_FILENAME + " TEXT, " +
+                    DB_TABLE_IMAGE_CONTEXT + " TEXT, " +
+                    DB_TABLE_IMAGE_DESCRIPTION + " TEXT, " +
+                    DB_TABLE_IMAGE_FLOORNUMBER + " INTEGER" +
+                ")";
+        db.execSQL(query);
+
+        query = "CREATE TABLE " + DB_TABLE_ACTIVITY + "(" +
+                    DB_TABLE_ACTIVITY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    DB_TABLE_ACTIVITY_OPENDAYDATE + " TEXT, " +
+                    DB_TABLE_ACTIVITY_STUDYNAME + " TEXT, " +
+                    DB_TABLE_ACTIVITY_STARTTIME + " TEXT, " +
+                    DB_TABLE_ACTIVITY_ENDTIME + " TEXT, " +
+                    DB_TABLE_ACTIVITY_CLASSROOM + " TEXT, " +
+                    DB_TABLE_ACTIVITY_INFORMATION + " TEXT" +
+                ")";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_OPENDAY);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_INSTITUTE);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_STUDY);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_LOCATION);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_IMAGE);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_ACTIVITY);
+        onCreate(db);
     }
 }
--- OpenDagApp/app/src/main/java/com/example/opendagapp/Homepage.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Homepage.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Homepage extends AppCompatActivity {
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        myDb = new DatabaseHelper(this);
     }
 }
