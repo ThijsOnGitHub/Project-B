7e27f43a72832f6b04d8a6fd75285faba967fe68
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-28 20:20:35 +0100

    .........................................

---
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Workshops.java
@@ -0,0 +1,33 @@
+package com.example.deopendagapp;
+
+public class Workshops {
+    private String roomcode, study, starttime, subject, timeleft;
+
+    public Workshops(String roomcode, String study, String starttime, String subject) {
+        this.subject = subject;
+        this.study = study;
+        this.roomcode = roomcode;
+        this.starttime = starttime;
+        this.timeleft = ""; // starttime - time.now()
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getStudy() {
+        return study;
+    }
+
+    public String getRoomcode() {
+        return roomcode;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public String getTimeleft() {
+        return timeleft;
+    }
+}
