5194d5b3b99f09bba6205ca3cf95d1be20aa9a16
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-28 17:33:44 +0100

    Make the program so usable and understandable
    make it so easy as possible but still an erro occured

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
@@ -11,116 +11,77 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.zip.Inflater;
 
-//rebuilt after https://stackoverflow.com/questions/16369814/how-to-access-the-drawable-resources-by-name-in-android
-class DrawableManager{
-    private static Context context = null;
-    private static Resources resources;
-
-    public DrawableManager(Context c) {
-        context = c;
-        resources=context.getResources();
-    }
-
-    //By myself
-    public static int getID(String name){
-        int resourceId = resources.getIdentifier(name, "drawable",
-                context.getPackageName());
-        return resourceId;
-    }
-
-    public static boolean nameExist(String name){
-        if (getID(name)==0){
-            return false;
-        }else{
-            return true;
-        }
-    }
-}
-
-class floorManager{
-    private DrawableManager getPic;
-    private String[] buildingsList= new String[] {"h99","wd103","wn107"};
-    private String building=buildingsList[0];
-    private int buildingNum=0;
-    private int floorNumber = 2;
-    private ImageView showFloor;
-
-    public floorManager(Context c,ImageView showFloor ){
-        getPic= new DrawableManager(c);
-        this.showFloor= showFloor;
-        updateImage();
-    }
-
-    public void changeBuilding(int amount){
-        buildingNum= stayBetweenIncl(0,2,buildingNum+amount);
-        building=buildingsList[buildingNum];
-        updateImage();
-    }
-
-    public void floorUp(){
-        floorNumber+=1;
-        updateImage();
-    }
-
-
-    private String createName(int floorNumber){
-        return building+Integer.toString(floorNumber)+"e";
-    }
-
-    private String createName(){
-        return building+Integer.toString(floorNumber)+"e";
-    }
-
-    public void updateImage(){
-        String name = createName();
-        int id=getPic.getID(name);
-        boolean test = R.drawable.h993e==id;
-        showFloor.setImageResource(id);
-
-    }
-
-    public void updateImage(int Id){
-        String name = createName();
-        int id=getPic.getID(name);
-        boolean test = R.drawable.h993e==id;
-        showFloor.setImageResource(id);
-    }
-    private int stayBetweenIncl(int min,int max,int number){
-        if (number<min){
-            return min;
-        }else if (number>max){
-            return max;
-        }
-        return number;
-    }
-}
 
 public class Map extends AppCompatActivity {
+
+    class floorManager{
+        public String[] buildingsList= new String[] {"h99","wd103","wn107"};
+        public String building=buildingsList[0];
+        public int buildingNum=0;
+        public int floor=0;
+        public Context context;
+        public ImageView floorView;
+        public TextView testView;
+
+        public floorManager(Context c,ImageView showFloor ,TextView testViewi){
+            context=c;
+            floorView=showFloor;
+            testView=testViewi;
+            updateImage();
+        }
+
+        public void changeBuilding(int amount){
+            buildingNum= stayBetweenIncl(0,2,buildingNum+amount);
+            building=buildingsList[buildingNum];
+            updateImage();
+        }
+
+        public void floorUp(ImageView showFloor){
+            floor+=1;
+            updateImage();
+        }
+
+        public String createName(){
+            return building+Integer.toString(floor)+"e";
+        }
+
+
+        public void updateImage(){
+            showFloor.setImageResource(0);
+            //https://stackoverflow.com/questions/16369814/how-to-access-the-drawable-resources-by-name-in-android
+            int id=context.getResources().getIdentifier(createName(),"drawable",context.getPackageName());
+            showFloor.setImageResource(id);
+            testView.setText(createName()+"\n"+id);
+        }
+
+        private int stayBetweenIncl(int min,int max,int number){
+            if (number<min){
+                return min;
+            }else if (number>max){
+                return max;
+            }
+            return number;
+        }
+    }
+
     floorManager floor;
-
-
+    ImageView showFloor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        ImageView showFloor = (ImageView) findViewById(R.id.showFloor);
-        floor = new floorManager(this,showFloor);
-        showFloor.setImageResource(R.drawable.h990e);
-        showFloor.setImageResource(R.drawable.h992e);
+        showFloor= findViewById(R.id.showFloor);
+        TextView testView = findViewById(R.id.TestView);
+        floor = new floorManager(this,showFloor,testView);
     }
 
-    public void clickFloorUp(View v){
+    public void clickUp(View v){
+        floor.floorUp(showFloor);
     }
-
-
-        /*
-        floor.updateImage();
-        floor.updateImage();
-        */
-
-
-
 }
