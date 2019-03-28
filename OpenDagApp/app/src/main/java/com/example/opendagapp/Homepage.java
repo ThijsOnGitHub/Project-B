4c507d06d0080001b1c22ca8324ce58c9d9a1f3f
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-28 22:55:45 +0100

    geprobeerd werkend te krijgen

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Homepage.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Homepage.java
@@ -1,13 +1,17 @@
 package com.example.opendagapp;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Homepage extends AppCompatActivity {
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        myDb = new DatabaseHelper(this);
     }
 }
