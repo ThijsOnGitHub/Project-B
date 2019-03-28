2c94c33b24399ea0a1a8bdb78fbeaabb01243e7a
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-03-28 20:03:57 +0100

    workshops files

---
+++ De Open dag app/app/src/main/java/com/example/deopendagapp/view_workshops.java
@@ -0,0 +1,46 @@
+package com.example.deopendagapp;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class view_workshops extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    private ListView listView_workshop;
+    private Workshoplistadapter adapter;
+    private List<Workshops> mWorkshoplist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_workshops);
+
+        myDb = new DatabaseHelper(this);
+
+        mWorkshoplist = new ArrayList<>();
+        listView_workshop = findViewById(R.id.listview);
+
+        viewData();
+    }
+
+    private void viewData(){
+        Cursor cursor = myDb.viewData();
+
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No data to show :(", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                mWorkshoplist.add(new Workshops(cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4),""));
+            }
+
+            adapter = new Workshoplistadapter(getApplicationContext(), mWorkshoplist);
+            listView_workshop.setAdapter(adapter);
+        }
+    }
+}
