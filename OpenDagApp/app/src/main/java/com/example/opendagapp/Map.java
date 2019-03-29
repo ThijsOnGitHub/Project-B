13de854231dee48997c9fe5da8ae9f2b0045b62e
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-29 16:23:13 +0100

    Added floors

--- OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/Map.java
@@ -2,20 +2,17 @@ package com.example.opendagapp;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.zip.Inflater;
+
 class DrawableManager{
     private static Context context = null;
     private static Resources resources;
@@ -26,18 +23,34 @@ class DrawableManager{
     }
 
     //By myself
-    public static int getID(String name){
+    public int getID(String name){
         int resourceId = resources.getIdentifier(name, "drawable",
                 context.getPackageName());
         return resourceId;
     }
 
-    public static boolean nameExist(String name){
+    public boolean nameExist(String name){
         if (getID(name)==0){
             return false;
         }else{
             return true;
         }
+    }
+}
+
+
+class AttributePack{
+    public Button upBut,downBut,leftBut,rightBut;
+    ImageView showFloor;
+    TextView floorIndicator;
+
+    public AttributePack(ImageView showFloorInvoer ,TextView floorIndicatorinvoer,Button upButton,Button downButton,Button leftButton,Button rightButton){
+        showFloor=showFloorInvoer;
+        floorIndicator=floorIndicatorinvoer;
+        upBut=upButton;
+        downBut=downButton;
+        leftBut=leftButton;
+        rightBut=rightButton;
     }
 }
 
@@ -47,17 +60,23 @@ class mapManager{
     public int buildingNum=0;
     public int floor=0;
     public Context context;
-    public ImageView showFloor;
-    public TextView testView;
     public DrawableManager getPic;
+
+
     public Button upButton,downButton,leftButton,rightButton;
+    ImageView showFloor;
+    TextView floorIndicator;
 
     //,Button upBut,Button downBut,Button leftBut,Button rightBut
-    public mapManager(Context c,ImageView showFloorInvoer ,TextView testViewi){
-        DrawableManager getPic = new DrawableManager(c);
+    public mapManager(Context c,AttributePack attributes){
+        getPic = new DrawableManager(c);
         context=c;
-        showFloor=showFloorInvoer;
-        testView=testViewi;
+        showFloor=attributes.showFloor;
+        floorIndicator=attributes.floorIndicator;
+        upButton=attributes.upBut;
+        downButton=attributes.downBut;
+        leftButton=attributes.leftBut;
+        rightButton=attributes.rightBut;
         updateImage();
     }
 
@@ -86,10 +105,46 @@ class mapManager{
 
 
     public void updateImage(){
+        updateVisabilatyButton();
         showFloor.setImageResource(0);
         int id=getPic.getID(createName());
         showFloor.setImageResource(id);
-        testView.setText(createName()+"\n");
+        floorIndicator.setText(createName()+"\n");
+    }
+
+    public void updateVisabilatyButton(){
+        setVisable(upButton,downButton,leftButton,rightButton);
+        if(!checkFloorExist(1,0)){
+            setInvisible(upButton);
+        }
+        if(!checkFloorExist(-1,0)){
+            setInvisible(downButton);
+        }
+        if(!checkFloorExist(0,-1)){
+            setInvisible(leftButton);
+        }
+        if(!checkFloorExist(0,1)){
+            setInvisible(rightButton);
+        }
+    }
+
+    private boolean checkFloorExist(int amountOfFloorHigher, int amountOfBuildingRight){
+        if (buildingNum+amountOfBuildingRight<0 ||buildingNum+amountOfBuildingRight>buildingsList.length-1){
+            return false;
+        }
+        return getPic.nameExist(createName(floor+amountOfFloorHigher,buildingsList[buildingNum+amountOfBuildingRight]));
+    }
+
+    private void setInvisible(Button... buttons){
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void setVisable(Button... buttons){
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setVisibility(View.VISIBLE);
+        }
     }
 
     private int stayBetweenIncl(int min,int max,int number){
@@ -101,17 +156,22 @@ class mapManager{
         return number;
     }
 }
-public class Map extends AppCompatActivity {
+
+
+public class Map extends AppCompatActivity{
     mapManager floor;
-    ImageView showFloor;
+    GestureDetector swipeListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        showFloor= findViewById(R.id.showFloor);
-        TextView testView = findViewById(R.id.TestView);
-        floor = new mapManager(this,showFloor,testView);
+        ImageView showFloor = findViewById(R.id.ImageView_showFloor);
+        AttributePack attributes = new AttributePack(showFloor, (TextView) findViewById(R.id.TextView_FloorIndicator), (Button) findViewById(R.id.Button_FloorUp), (Button) findViewById(R.id.Button_FloorDown), (Button) findViewById(R.id.Button_BuildingLeft), (Button) findViewById(R.id.Button_BuildingRight));
+        floor = new mapManager(this, attributes);
     }
+
 
     public void clickUp(View v){
         floor.changeFloor(1);
@@ -128,4 +188,6 @@ public class Map extends AppCompatActivity {
     public void clickRight(View v){
         floor.changeBuilding(1);
     }
+
+
 }
