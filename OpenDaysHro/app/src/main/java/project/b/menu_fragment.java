a79fa2992814daa7ef13438ea2e7c1051fbbd7ae
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-31 15:11:56 +0200

    Add top Bar

--- OpenDaysHro/app/src/main/java/project/b/menu_fragment.java
+++ OpenDaysHro/app/src/main/java/project/b/menu_fragment.java
@@ -1,17 +1,11 @@
 package project.b;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 
 
 public class menu_fragment extends Fragment implements View.OnClickListener{
