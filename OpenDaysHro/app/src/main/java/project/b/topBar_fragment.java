7e1ac6c8451757daa5712e3436943366ed5d7a9d
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-31 15:17:03 +0200

    Update topBar_fragment.java

--- OpenDaysHro/app/src/main/java/project/b/topBar_fragment.java
+++ OpenDaysHro/app/src/main/java/project/b/topBar_fragment.java
@@ -15,8 +15,10 @@ public class topBar_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.top_bar, container, false);
+        View view=inflater.inflate(R.layout.top_bar, container, false);
+        //put code here
+
+        return view;
     }
 
 
