dc939687beebf577258ff266147c8fa15ac00993
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-31 14:38:58 +0200

    Fix the error

--- OpenDaysHro/app/src/main/java/project/b/menu_fragment.java
+++ OpenDaysHro/app/src/main/java/project/b/menu_fragment.java
@@ -4,14 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 
-public class menu_fragment extends Fragment {
-    public void swapPage(View v) {
+public class menu_fragment extends Fragment implements View.OnClickListener{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.activity_menu, container, false);
+        view.findViewById(R.id.menuItem_homepage).setOnClickListener(this);
+        view.findViewById(R.id.menuItem_education).setOnClickListener(this);
+        view.findViewById(R.id.menuItem_map).setOnClickListener(this);
+        view.findViewById(R.id.menuItem_openDays).setOnClickListener(this);
+        view.findViewById(R.id.menuItem_contact).setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
         Intent swapToHome = new Intent(getActivity(), MainActivity.class);
         swapToHome.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         Intent swapToEducation = new Intent(getActivity(), educations_activity.class);
@@ -41,5 +59,4 @@ public class menu_fragment extends Fragment {
                 break;
         }
     }
-
 }
