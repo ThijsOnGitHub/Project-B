ccc3d76a4ad74fa7ce758ca81d24939f6898a13a
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-03-31 14:25:29 +0200

    Remove Unnessarry code

--- OpenDaysHro/app/src/main/java/project/b/MainActivity.java
+++ OpenDaysHro/app/src/main/java/project/b/MainActivity.java
@@ -15,29 +15,4 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-    public void swapPages(View v){
-        Intent swapToHome = new Intent(this,MainActivity.class); swapToHome.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        Intent swapToEducation = new Intent(this,educations_activity.class); swapToEducation.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        Intent swapToMap = new Intent(this,map_activity.class); swapToMap.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        Intent swapToOpenDays = new Intent(this,opendays_activity.class); swapToOpenDays.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        Intent swapToContact = new Intent(this,contact_activity.class); swapToContact.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-
-        switch (v.getId()){
-            case R.id.menuItem_homepage:
-                startActivity(swapToHome);
-                break;
-            case R.id.menuItem_education:
-                startActivity(swapToEducation);
-                break;
-            case R.id.menuItem_map:
-                startActivity(swapToMap);
-                break;
-            case R.id.menuItem_openDays:
-                startActivity(swapToOpenDays);
-                break;
-            case R.id.menuItem_contact:
-                startActivity(swapToContact);
-                break;
-        }
-    }
 }
