a1c57bd992dd4a42c90bd4dbc198d94164144195
christianvanos <47158164+christianvanos@users.noreply.github.com>
2019-04-01 15:09:58 +0200

    insert data for the tables

--- OpenDagApp/app/src/main/java/com/example/opendagapp/DatabaseHelper.java
+++ OpenDagApp/app/src/main/java/com/example/opendagapp/DatabaseHelper.java
@@ -1,25 +1,27 @@
 package com.example.opendagapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.strictmode.SqliteObjectLeakedViolation;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DB_DATABASE_HROOPENDAY_VERSION = 1;
-    private static final String DB_DATABASE_HROOPENDAY = "hro_opendays.db";
+    private static final String DB_DATABASE_HROOPENDAY = "hro_openday.db";
 
     public static final String DB_TABLE_OPENDAY = "openday";
     public static final String DB_TABLE_OPENDAY_ID = "id"; // Primary key, autoincrement
     public static final String DB_TABLE_OPENDAY_DATE = "date"; // text
-    public static final String DB_TABLE_OPENDAY_STARTTIME = "start_time"; // text
-    public static final String DB_TABLE_OPENDAY_ENDTIME = "end_time"; // text
+    public static final String DB_TABLE_OPENDAY_STARTTIME = "starttime"; // text
+    public static final String DB_TABLE_OPENDAY_ENDTIME = "endtime"; // text
     public static final String DB_TABLE_OPENDAY_INSTITUTEFULLNAME = "institute_fullname"; // text
 
     public static final String DB_TABLE_INSTITUTE = "institute";
     public static final String DB_TABLE_INSTITUTE_ID = "id"; // Primary key, autoincrement
-    public static final String DB_TABLE_INSTITUTE_FULLNAME = "full_name"; // Text
-    public static final String DB_TABLE_INSTITUTE_SHORTNAME = "short_name"; // Text
-    public static final String DB_TABLE_INSTITUTE_GENERALINFORMATION = "general_information"; // Text
+    public static final String DB_TABLE_INSTITUTE_FULLNAME = "fullname"; // Text
+    public static final String DB_TABLE_INSTITUTE_SHORTNAME = "shortname"; // Text
+    public static final String DB_TABLE_INSTITUTE_GENERALINFORMATION = "generalinformation"; // Text
 
     public static final String DB_TABLE_STUDY = "study";
     public static final String DB_TABLE_STUDY_ID = "id"; // Primary key, autoincrement
@@ -32,16 +34,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DB_TABLE_ACTIVITY_ID = "id"; // Primary key, autoincrement
     public static final String DB_TABLE_ACTIVITY_OPENDAYDATE = "openday_date"; // text
     public static final String DB_TABLE_ACTIVITY_STUDYNAME = "study_name"; // text
-    public static final String DB_TABLE_ACTIVITY_STARTTIME = "start_time"; // text
-    public static final String DB_TABLE_ACTIVITY_ENDTIME = "end_time"; // text
+    public static final String DB_TABLE_ACTIVITY_STARTTIME = "starttime"; // text
+    public static final String DB_TABLE_ACTIVITY_ENDTIME = "endtime"; // text
     public static final String DB_TABLE_ACTIVITY_CLASSROOM = "classroom"; // text
     public static final String DB_TABLE_ACTIVITY_INFORMATION = "information"; // text
 
     public static final String DB_TABLE_LOCATION = "location";
     public static final String DB_TABLE_LOCATION_ID = "id"; // Primary key, autoincrement
     public static final String DB_TABLE_LOCATION_ADDRESS = "address"; // text
-    public static final String DB_TABLE_LOCATION_ZIPCODE = "zip_code"; // text
-    public static final String DB_TABLE_LOCATION_PHONENUMBER = "phone_number"; // text
+    public static final String DB_TABLE_LOCATION_ZIPCODE = "zipcode"; // text
+    public static final String DB_TABLE_LOCATION_PHONENUMBER = "phonenumber"; // text
     public static final String DB_TABLE_LOCATION_IMAGEDESCRIPRION = "image_description"; // text
 
     public static final String DB_TABLE_IMAGE = "image";
@@ -49,7 +51,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DB_TABLE_IMAGE_FILENAME = "filename"; // text
     public static final String DB_TABLE_IMAGE_CONTEXT = "context"; // text
     public static final String DB_TABLE_IMAGE_DESCRIPTION = "description"; // text
-    public static final String DB_TABLE_IMAGE_FLOORNUMBER = "floor_number"; // text
+    public static final String DB_TABLE_IMAGE_FLOORNUMBER = "floornumber"; // text
 
 
     public DatabaseHelper(Context context) {
@@ -75,5 +77,90 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_LOCATION);
         db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_IMAGE);
         onCreate(db);
+    }
+
+    public boolean createOpenday(String date, String starttime, String endtime, String institute_fullname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_OPENDAY_DATE, date);
+        contentValues.put(DB_TABLE_OPENDAY_STARTTIME, starttime);
+        contentValues.put(DB_TABLE_OPENDAY_ENDTIME, endtime);
+        contentValues.put(DB_TABLE_OPENDAY_INSTITUTEFULLNAME, institute_fullname);
+
+        long result = db.insert(DB_TABLE_OPENDAY, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
+    }
+
+    public boolean createInstitute(String fullname, String shortname, String generalinformation) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_INSTITUTE_FULLNAME, fullname);
+        contentValues.put(DB_TABLE_INSTITUTE_SHORTNAME, shortname);
+        contentValues.put(DB_TABLE_INSTITUTE_GENERALINFORMATION, generalinformation);
+
+        long result = db.insert(DB_TABLE_INSTITUTE, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
+    }
+
+    public boolean createStudy(String institute_fullname, String name, String type, String location_address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_STUDY_INSTITUTEFULLNAME, institute_fullname);
+        contentValues.put(DB_TABLE_STUDY_NAME, name);
+        contentValues.put(DB_TABLE_STUDY_TYPE, type);
+        contentValues.put(DB_TABLE_STUDY_LOCATIONADRESS, location_address);
+
+        long result = db.insert(DB_TABLE_STUDY, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
+    }
+
+    public boolean createActivity(String openday_date, String study_name, String starttime, String endtime, String classroom, String information) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_ACTIVITY_OPENDAYDATE, openday_date);
+        contentValues.put(DB_TABLE_ACTIVITY_STUDYNAME, study_name);
+        contentValues.put(DB_TABLE_ACTIVITY_STARTTIME, starttime);
+        contentValues.put(DB_TABLE_ACTIVITY_ENDTIME, endtime);
+        contentValues.put(DB_TABLE_ACTIVITY_CLASSROOM, classroom);
+        contentValues.put(DB_TABLE_ACTIVITY_INFORMATION, information);
+
+        long result = db.insert(DB_TABLE_ACTIVITY, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
+    }
+
+    public boolean createLocation(String address, String zipcode, String phonenumber, String image_description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_LOCATION_ADDRESS, address);
+        contentValues.put(DB_TABLE_LOCATION_ZIPCODE, zipcode);
+        contentValues.put(DB_TABLE_LOCATION_PHONENUMBER, phonenumber);
+        contentValues.put(DB_TABLE_LOCATION_IMAGEDESCRIPRION, image_description);
+
+        long result = db.insert(DB_TABLE_LOCATION, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
+    }
+
+    public boolean createImage(String filename, String context, String description, String floornumber) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DB_TABLE_IMAGE_FILENAME, filename);
+        contentValues.put(DB_TABLE_IMAGE_CONTEXT, context);
+        contentValues.put(DB_TABLE_IMAGE_DESCRIPTION, description);
+        contentValues.put(DB_TABLE_IMAGE_FLOORNUMBER, floornumber);
+
+        long result = db.insert(DB_TABLE_IMAGE, null, contentValues);
+        db.close();
+        return result != -1; // if result == true then the values are inserted
     }
 }
