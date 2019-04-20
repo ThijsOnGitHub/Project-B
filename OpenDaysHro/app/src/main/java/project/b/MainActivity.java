21f4067e8f879d79f17ad9ed1a3667df728f6eca
caslay oort <caslayoort@gmail.com>
2019-04-20 20:54:00 +0200

    added an object called 'appHelper'

--- OpenDaysHro/app/src/main/java/project/b/MainActivity.java
+++ OpenDaysHro/app/src/main/java/project/b/MainActivity.java
@@ -1,65 +1,30 @@
 package project.b;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.content.pm.ActivityInfo;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends appHelper {
 
+    DatabaseHelper myDatabase;
+    LayoutHelper layout;
 
+    int numOfListItems = 20;
+    int imageCounter = 0;
 
     int[] drawables = new int[]{R.drawable.beginning_by_ryky,R.drawable.best_friends_by_ryky,R.drawable.bffs_by_synderen,R.drawable.beginning_of_love_by_ryky,R.drawable.better_day_by_ryky,R.drawable.beyond_by_auroralion};
 
-    int imageCounter = 0;
     ImageView mainImage;
-
-
-    // https://stackoverflow.com/questions/3204852/android-add-a-textview-to-linear-layout-programmatically
-
-    public void addToLayout(String object, String... args){
-        switch (object){
-            case "ListItem":
-                LinearLayout myLLayout = new LinearLayout(this);
-                RelativeLayout myRLayoutLarge = new RelativeLayout(this);
-                RelativeLayout myRLayoutSmall = new RelativeLayout(this);
-                TextView myTextview = new TextView(this);
-                Button myButton = new Button(this);
-
-                myTextview.setText(args[0]); myTextview.setGravity(Gravity.CENTER);
-                myButton.setText(args[1]);
-                myLLayout.setBackgroundColor(getResources().getColor(R.color.white));
-                myButton.setBackgroundColor(getResources().getColor(R.color.hro_red)); myButton.setTextColor(getResources().getColor(R.color.text));
-                LinearLayout.LayoutParams mainLLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200);
-                mainLLayout.setMargins(0,50,0,0);
-
-                myLLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-                myLLayout.setLayoutParams(mainLLayout);
-                myRLayoutLarge.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1));
-                myRLayoutSmall.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,2));
-                myTextview.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
-                myButton.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
-
-                ((RelativeLayout)myRLayoutLarge).addView((TextView)myTextview);
-                ((RelativeLayout)myRLayoutSmall).addView((Button)myButton);
-                ((LinearLayout)myLLayout).addView((RelativeLayout)myRLayoutLarge);
-                ((LinearLayout)myLLayout).addView((RelativeLayout)myRLayoutSmall);
-
-                LinearLayout main = (LinearLayout)findViewById(R.id.hallo_wereld); ((LinearLayout)main).addView((LinearLayout)myLLayout);
-        }
-    }
-
-
-
-    DatabaseHelper myDatabase;
 
 
     @Override
@@ -73,14 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
         mainImage = (ImageView)findViewById(R.id.mainImage);
 
+        layout = new LayoutHelper(this);
 
-        int numOfListItems = 5;
-
-        for(int i = 0; i < numOfListItems; i++) {
-            addToLayout("ListItem", "Informatica\n04/04/2019\nWijnhaven 107", "Options");
+        for (int i = 0; i < numOfListItems; i++) {
+            layout.ListItem_openday("Informatica","Wijnhaven 107","8:00\nStart over: 20 min",R.id.hallo_wereld);
         }
-        
+
     }
+
+
 
 
 
---
+++ OpenDaysHro/app/src/main/java/project/b/appHelper.java
@@ -0,0 +1,77 @@
+package project.b;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class appHelper extends AppCompatActivity {
+
+    // https://stackoverflow.com/questions/3204852/android-add-a-textview-to-linear-layout-programmatically
+
+    public class LayoutHelper {
+        Context context;
+
+        int menuColor = R.color.dark_grey;
+
+        public LayoutHelper(Context context){ this.context = context; }
+
+        public void goToOtherPageWithInformation(String[] info) { System.out.println(info); }
+
+        public void ListItem_openday( String ListItem_Description, String ListItem_Location, String ListItem_Time , int addToThisLayout) {
+            LinearLayout LinearLayout_main = new LinearLayout(this.context);
+                LinearLayout_main.setOrientation(LinearLayout.HORIZONTAL);
+                LinearLayout_main.isClickable();
+                LinearLayout_main.setBackgroundColor(getResources().getColor(menuColor));
+                LinearLayout.LayoutParams LinearLayout_main_layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200);
+                    LinearLayout_main_layoutParams.setMargins(0,0,0,0);
+                    LinearLayout_main.setLayoutParams(LinearLayout_main_layoutParams);
+
+            RelativeLayout listItem_description_layout = new RelativeLayout(this.context);
+                listItem_description_layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1));
+
+            RelativeLayout listItem_loc = new RelativeLayout(this.context);
+                listItem_loc.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,2));
+
+            RelativeLayout listItem_time = new RelativeLayout(this.context);
+                listItem_time.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,2));
+
+            TextView listItem_description = new TextView(this.context);
+                listItem_description.setText(ListItem_Description); listItem_description.setGravity(Gravity.CENTER);
+                listItem_description.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+
+            TextView listItem_Location = new TextView(this.context);
+                listItem_Location.setText(ListItem_Location); listItem_description.setGravity(Gravity.CENTER);
+                listItem_Location.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+
+            TextView listItem_Time = new TextView(this.context);
+                listItem_Time.setText(ListItem_Time); listItem_description.setGravity(Gravity.CENTER);
+                listItem_Time.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT));
+
+
+            ((RelativeLayout) listItem_description_layout ).addView((TextView) listItem_description);
+                ((LinearLayout)LinearLayout_main).addView((RelativeLayout)listItem_description_layout);
+            ((RelativeLayout) listItem_loc).addView(listItem_Location);
+                ((LinearLayout)LinearLayout_main).addView((RelativeLayout)listItem_loc);
+            ((RelativeLayout) listItem_time).addView(listItem_Time);
+                ((LinearLayout)LinearLayout_main).addView((RelativeLayout)listItem_time);
+
+            String[] infoToPass = {ListItem_Description, ListItem_Location, ListItem_Time};
+            //LinearLayout_main.callOnClick( goToOtherPageWithInformation(infoToPass) );
+
+            LinearLayout main = (LinearLayout)findViewById( addToThisLayout );
+                ((LinearLayout)main).addView((LinearLayout)LinearLayout_main);
+
+            if (this.menuColor == R.color.light_grey) { this.menuColor = R.color.dark_grey; } else { this.menuColor = R.color.light_grey; }
+
+        }
+    }
+}
