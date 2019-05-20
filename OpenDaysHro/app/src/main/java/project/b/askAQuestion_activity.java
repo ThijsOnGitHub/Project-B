ad50ff8e04ef47dd8b69f11b1d466185912234c6
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-05-20 15:07:12 +0200

    change menu bar ask question page

--- OpenDaysHro/app/src/main/java/project/b/askAQuestion_activity.java
+++ OpenDaysHro/app/src/main/java/project/b/askAQuestion_activity.java
@@ -29,8 +29,18 @@ public class askAQuestion_activity extends appHelper {
         textFieldView=findViewById(R.id.editText_TextField_ContactForm);
         emailView=findViewById(R.id.editText_E_mail_ContactForm);
 
-        layout= new LayoutHelper(this);
-        
+        layout = new LayoutHelper(this);
+
+        Intent home = new Intent(getBaseContext(), MainActivity.class);
+        Intent educations = new Intent(getBaseContext(), educations_activity.class);
+        Intent about_cmi = new Intent(getBaseContext(), About_activity.class);
+        Intent contact = new Intent(getBaseContext(), contact_activity.class);
+
+        Intent[] myIntents = new Intent[]{home,educations,about_cmi,contact};
+        int[] images = new int[]{R.drawable.ic_home_white_24dp,R.drawable.baseline_school_24px,R.drawable.ic_location_city_white_24dp,R.drawable.ic_chat_grey_24dp};
+        String[] text = new String[]{"home","Study programs","About CMI","Contact"};
+
+        layout.generate_menu(R.id.menu_bar,images,text,myIntents);
     }
 
 
