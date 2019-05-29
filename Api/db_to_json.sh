fda823d908984b363fc624c2f10d151038d0887e
caslay oort <caslayoort@gmail.com>
2019-05-29 10:04:28 +0200

    added the functionality to get the table fields of the database to the bash program

--- Api/db_to_json.sh
+++ Api/db_to_json.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
 db='DATABASE'
+mysql_user="USER"
+
 data_folder="data_database_${db}"
+extention_data="_data"
+extention_field="_field"
+extention_data_fields="_data_field"
+
+### PREPAIR PROGRAM ###
 mkdir ${data_folder}
 
-#echo "SHOW TABLES FROM ${db};" | mysql -u USER
+if $( hash jq ); then
+        echo "-------------------------------- GENERATING JSON --------------------------------"
+else
+        apt-get install jq
+        echo "-------------------------------- GENERATING JSON --------------------------------"
+fi
 
-touch tableName.sql
-
-echo "USE ${db};" >> tableName.sql
-echo "SHOW TABLES;" >> tableName.sql
-
-
+### GETTING DB TABLES ###
+touch tableName.sql; echo "USE ${db}; SHOW TABLES;" >> tableName.sql
 
 touch tableName.txt
-for table_name in $( cat tableName.sql | mysql -u USER ); do
+for table_name in $( cat tableName.sql | mysql -u ${mysql_user} ); do
         echo "${table_name}" >> tableName.txt
 done
 
-rm tableName.sql
+rm tableName.sql; sed '1d' tableName.txt > tmpfile; mv tmpfile tableName.txt
 
-
-
-sed '1d' tableName.txt > tmpfile; mv tmpfile tableName.txt
-
-touch select.sql
-
+### GETTING TABLE FIELDS ###
 for table_name in $( cat tableName.txt ); do
-        echo "USE ${db}; SELECT * FROM ${table_name};" > select.sql
-        cat select.sql | mysql -u USER >> ${data_folder}/${table_name}_data.txt
+        echo "SELECT COLUMN_NAME FROM information_schema.columns WHERE table_name = '${table_name}' AND table_schema = '${db}';" > select.sql
+        cat select.sql | mysql -u ${mysql_user} >> ${data_folder}/${table_name}${extention_fields}.txt
+        sed '1d' ${data_folder}/${table_name}${extention_fields}.txt > tmpfile; mv tmpfile ${data_folder}/${table_name}${extention_fields}.txt
 done
 rm select.sql
 
-for data in $( ls ${data_folder}/ ); do
-        cat ${data_folder}/${data}
-done
 
-rm -r ${data_folder}/
-rm tableName.txt
+rm -r ${data_folder}/; rm tableName.txt
+
