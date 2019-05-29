936935deeb75f566ee99d6ba7478d1f6a8af9fff
caslay oort <caslayoort@gmail.com>
2019-05-29 10:00:11 +0200

    added the function to get the data from the database (not in json yet) to the bash program

--- Api/db_to_json.sh
+++ Api/db_to_json.sh
@@ -4,7 +4,7 @@ db='DATABASE'
 data_folder="data_database_${db}"
 mkdir ${data_folder}
 
-#echo "SHOW TABLES FROM ${db};" | mysql -u local
+#echo "SHOW TABLES FROM ${db};" | mysql -u USER
 
 touch tableName.sql
 
@@ -19,6 +19,22 @@ for table_name in $( cat tableName.sql | mysql -u USER ); do
 done
 
 rm tableName.sql
+
+
+
+sed '1d' tableName.txt > tmpfile; mv tmpfile tableName.txt
+
+touch select.sql
+
+for table_name in $( cat tableName.txt ); do
+        echo "USE ${db}; SELECT * FROM ${table_name};" > select.sql
+        cat select.sql | mysql -u USER >> ${data_folder}/${table_name}_data.txt
+done
+rm select.sql
+
+for data in $( ls ${data_folder}/ ); do
+        cat ${data_folder}/${data}
+done
 
 rm -r ${data_folder}/
 rm tableName.txt
